41854b971660416bc2df7d6c8db18ca9f3a8e248
Durga Thakur <durga.thakur.ui@gmail.com>
2021-10-15 18:20:54 +0400

    Pet finder project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import DogList from "./containers/DogList/DogList";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div className={'wrapper'}>
+        <DogList />
+      </div>
+    </>
+  );
+}
+
+export default App;
